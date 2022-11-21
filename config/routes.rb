a45ad3caa7931c2aff8f446202cfb22f34aea9ac
Christopher Beraud <beraud.christopher@gmail.com>
2022-11-21 17:53:52 +0100

    added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  # Defines the root path route ("/")
+  # As a visitor I can visit the home page
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # 
+
 end
