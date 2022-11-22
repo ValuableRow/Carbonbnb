859e0fa53950d524b606bcea26eff616396935c1
Christopher Beraud <beraud.christopher@gmail.com>
2022-11-22 18:01:37 +0100

    index page with sorting functions

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,4 +1,12 @@
 class Flat < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_many :ratings, through: :bookings
+
+  def average_rating
+    unless ratings.empty?
+      flat_ratings = ratings.map { |rating| rating.rating }
+      flat_ratings.sum.to_f / flat_ratings.count
+    end
+  end
 end
