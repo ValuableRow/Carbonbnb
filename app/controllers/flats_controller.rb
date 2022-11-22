01a8873421d14d63024b2fb8e5752814de69cd7a
Paul Tilly <paul.tilly88@gmail.com>
2022-11-22 12:20:33 +0100

    flats page

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,16 @@
 class FlatsController < ApplicationController
+  def new
+    @flat = Flat.new
+  end
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flats = Flat.find(params[:id])
+  end
+
+  def edit
+  end
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,4 +1,10 @@
 class Flat < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :photos, presence: true
+  validates :location, presence: true
+  validates :capacity, presence: true
+  validates :price_per_night, presence: true
 end
