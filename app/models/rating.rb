112e087ed575d55f1e0ef59d3fe8ba552b6dd492
Christopher Beraud <beraud.christopher@gmail.com>
2022-11-23 16:17:28 +0100

    added UI changes

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,19 +1,3 @@
 class Rating < ApplicationRecord
   belongs_to :booking
-  before_action :set_flat, only: %i[new, create]
-  def new
-    @flat = Flat.find(params[:flat_id])
-    @rating = Rating.new
-  end
-
-  def create
-    @rating = Rating.new(rating_params)
-    @rating.flat = @flat
-    @rating.save
-    redirect_to flat_path[@flat]
-  end
-
-  private
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # As a visitor I can browse through flats
-  resources :flats, only: [:index, :show] do
-    resources :ratings, only: [:new, :create]
+  resources :flats, only: %i[index show] do
+    # As a user I can create a rating of a flat
+    resources :bookings, only: %i[new create]
+    resources :ratings, only: :create
   end
 end
