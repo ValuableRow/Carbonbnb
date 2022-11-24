01cd958a1ba85005528154f1cc6f654240943e33
Alexander Belski <sascha.belski@gmail.com>
2022-11-24 16:28:34 +0100

    Code added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
+require 'date'
+
 class UsersController < ApplicationController
 
     def show
       @user = User.find(params[:id])
       @role = role(@user)
-    end
+      @redir = params[:redir_param].nil? ? 0 : params[:redir_param].to_i
+      # booking
+      @bookings_current = @user.bookings.where('end_date > ? ', Date.today)
+      @bookings_historic = @user.bookings.where('end_date <= ? ', Date.today)
+       # orders
+        @orders_current =  Booking.joins(:flat).where('flats.user_id = ? ', @user.id).where('bookings.end_date > ? ', Date.today)
+        @orders_historic =  Booking.joins(:flat).where('flats.user_id = ? ', @user.id).where('bookings.end_date <= ? ', Date.today)
+       end
    
     private
 
