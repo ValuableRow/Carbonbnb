1a313e54683b51bec0dffeedec74b4114a5c699d
Alexander Belski <sascha.belski@gmail.com>
2022-11-25 08:00:18 +0100

    Code added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,15 @@ class UsersController < ApplicationController
       # booking
       @bookings_current = @user.bookings.where('end_date > ? ', Date.today)
       @bookings_historic = @user.bookings.where('end_date <= ? ', Date.today)
-       # orders
-        @orders_current =  Booking.joins(:flat).where('flats.user_id = ? ', @user.id).where('bookings.end_date > ? ', Date.today)
-        @orders_historic =  Booking.joins(:flat).where('flats.user_id = ? ', @user.id).where('bookings.end_date <= ? ', Date.today)
+      # orders
+      @orders_current =  Booking.joins(:flat).where('flats.user_id = ? ', @user.id).where('bookings.end_date > ? ', Date.today)
+      @orders_historic =  Booking.joins(:flat).where('flats.user_id = ? ', @user.id).where('bookings.end_date <= ? ', Date.today)
+      # empties? 
+      @bookings_current_exist = @bookings_current.count > 0 ? true : false
+      @bookings_historic_exist = @bookings_historic.count > 0 ? true : false
+      @orders_current_exist = @orders_current.count > 0 ? true : false
+      @orders_historic_exist = @orders_historic.count > 0 ? true : false
+
        end
    
     private
