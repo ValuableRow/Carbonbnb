478f5437a5ca1d2d14c9e41fe09b8e156f3eb329
Paul Tilly <paul.tilly88@gmail.com>
2022-11-25 12:03:16 +0100

    fix: carbon footprint

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_11_24_153031) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,9 +65,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_153031) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "carbon_footprint"
     t.float "latitude"
     t.float "longitude"
+    t.float "carbon_footprint"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
